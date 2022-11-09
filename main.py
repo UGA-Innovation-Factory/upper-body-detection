238706eca78e43163fad990618354cf0153c250b
HypePhilosophy <jeffs.mao@gmail.com>
2022-11-08 23:07:45 -0500

    git init

---
+++ main.py
@@ -0,0 +1,15 @@
+from upper_body_detection import upper_body_detection
+
+
+# -----------------------------------------------------
+# Imports
+# -----------------------------------------------------
+
+
+def execute():
+    print("Initializing Camera...")
+    upper_body_detection()
+
+
+if __name__ == "__main__":
+    execute()
